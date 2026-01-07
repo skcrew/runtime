b097c56ac309d6372eeeba84fce6099e90ad4fef
Razu KC <kc.razu@gmail.com>
2026-01-07 22:42:08 +0545

    chore:remove dist folder

--- dist/plugins/ConfigPlugin.d.ts
+++
@@ -1,6 +0,0 @@
-import { PluginDefinition } from '../types.js';
-export interface ConfigPluginOptions {
-    validate?: (config: any) => boolean | Promise<boolean>;
-}
-export declare const ConfigPlugin: PluginDefinition;
-//# sourceMappingURL=ConfigPlugin.d.ts.map
